d96b82b1a7d0f1c992e63395873e997227b1448d
santicalle1 <santiagoferreyra@alumnos.itr3.edu.ar>
2023-10-10 19:14:22 -0300

    carrito 10/10/23

--- app/Views/carrito.php
+++ app/Views/carrito.php
@@ -39,6 +39,31 @@
                     </li>
                 </ul>
             </nav>
+<!-- paypal -->
+<body>
+<div id="paypal-button-container">
+
+</div>
+<script src="https://www.paypal.com/sdk/js?client-id=AUAdq8u-Z7OXMdzHDYgxOG6GxBpQGD8vJxHOn_GgRiTbLNZdQImfjM-TDg_iwRCLdwvRkb63dH77Cxd-&currency=USD"></script>
+<script>
+    paypal.Buttons({
+    style:{
+        color:'blue',
+        shape: 'pill'
+    },
+    createOrder: function(data, actions){
+        return actions.order.create({
+        purchase_units: [{
+            amount: {
+            value: 100
+        }
+        }]
+    })
+    }
+}).render('#paypal-button-container');
+</script>
+</body>
+<!-- paypal -->
             <footer>
                 <p class="texto-footer">© 2023 GymRats</p>
             </footer>
