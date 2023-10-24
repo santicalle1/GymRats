0e704b4684758c128a62be2e299c0e87adebf525
santicalle1 <santiagoferreyra@alumnos.itr3.edu.ar>
2023-10-24 01:54:06 -0300

    24/10/2023 login y header

--- app/Views/login.php
+++ app/Views/login.php
@@ -6,7 +6,7 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-image: url('<?= base_url('img/1.jpg') ?>');
+            background-image: url('<?= base_url('img/1.jpeg') ?>');
             /* Ruta de tu imagen de fondo */
             background-size: cover;
             background-repeat: no-repeat;
@@ -19,105 +19,8 @@
             align-items: center;
             min-height: 100vh;
         }
-
-        form {
-            max-width: 600px;
-            /* Aumenta el ancho del contenedor */
-            padding: 45px;
-            /* Aumenta el espacio interior */
-            background-color: rgba(255, 255, 255, 0.8);
-            /* Fondo blanco semi-transparente */
-            border-radius: 5px;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
-            text-align: center;
-        }
-
-        h1 {
-            font-size: 24px;
-            color: #333;
-            margin-bottom: 20px;
-        }
-
-        label {
-            display: block;
-            font-weight: bold;
-            margin-bottom: 10px;
-            color: #555;
-        }
-
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 20px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            font-size: 16px;
-        }
-
-        button[type="submit"] {
-            background-color: blue;
-            color: #fff;
-            border: none;
-            padding: 10px 20px;
-            cursor: pointer;
-            border-radius: 4px;
-            font-weight: bold;
-            font-size: 18px;
-            transition: background-color 0.3s ease;
-        }
-
-        button[type="submit"]:hover {
-            background-color: blue;
-        }
-
-        .error {
-            color: red;
-            font-size: 14px;
-            margin-top: 10px;
-            margin-bottom: 10px;
-            font-weight: bold;
-            text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
-            /* Agregar sombra al texto de error */
-        }
-
-        p.error-message {
-            background-color: red;
-            color: white;
-            padding: 10px;
-            border-radius: 5px;
-            margin-top: 10px;
-            text-align: center;
-        }
-
-        .register-link {
-            margin-top: 10px;
-            text-align: center;
-        }
-
-        .register-link a {
-            text-decoration: none;
-            color: blue;
-            padding: 4px 10px;
-            border: 2px solid blue;
-            border-radius: 4px;
-            transition: background-color 0.3s, color 0.3s, border-color 0.3s;
-        }
-
-        .register-link a:hover {
-            background-color: blue;
-            color: #fff;
-        }
-        .terms-link {
-    background-color: #fff; /* Cambia el color de fondo a tu elección */
-    color: blue; /* Cambia el color del texto a tu elección */
-    text-decoration: none;
-    padding: 4px 10px;
-    border: 2px solid #blue; /* Puedes ajustar el color del borde si lo deseas */
-    border-radius: 4px;
-    transition: background-color 0.3s, color 0.3s, border-color 0.3s;
-}
     </style>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
@@ -134,7 +37,8 @@
 
         <button type="submit">Iniciar Sesión</button>
         <div class="register-link">
-            <p>¿No tienes cuenta? <a href="<?= base_url("/register"); ?>">Regístrate</a></p>
+            <p>¿No tienes cuenta? </p>
+            <a href="<?= base_url("/register"); ?>">Regístrate</a>
             <a class="terms-link" href="<?= base_url("/terminos"); ?>">Términos y Condiciones</a>
         </div>
     </form>
