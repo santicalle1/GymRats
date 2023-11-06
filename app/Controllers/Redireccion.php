8b544237126a198912490593c737761215f2a70f
Your Name <you@example.com>
2023-11-06 19:14:27 -0300

    6/11/2023

--- app/Controllers/Redireccion.php
+++ app/Controllers/Redireccion.php
@@ -3,91 +3,57 @@ namespace App\Controllers;
 
 use App\Models\ContactModel;
 
-class Redireccion extends BaseController
-{
-    public function tienda($url)
-    {
-        $session = session();
-        $usuario = $session->get('usuario');
+namespace App\Controllers;
 
-        if ($usuario) {
-            // Si el usuario está logueado, redirigir a la vista de tienda
-            return redirect()->to(base_url('tienda'));
-        } else {
-            // Si no está logueado, guardar la URL de destino en la sesión
-            $session->set('url_destino', $url);
+namespace App\Controllers;
 
-            // Redirigir a la página de inicio de sesión
-            return redirect()->to(base_url('login'));
-        }
-}
-    public function carrito($url)
-    {
-        $session = session();
-        $usuario = $session->get('usuario');
-
-        if ($usuario) {
-            // Si el usuario está logueado, redirigir a la vista de tienda
-            return redirect()->to(base_url('carrito'));
-        } else {
-            // Si no está logueado, redirigir a la página de inicio de sesión
-            return redirect()->to(base_url('login'));
-        }
-    }
-    public function rutinas($url)
-    {
-        $session = session();
-        $usuario = $session->get('usuario');
-
-        if ($usuario) {
-            // Si el usuario está logueado, redirigir a la vista de rutinas
-            return redirect()->to(base_url('rutinas'));
-        } else {
-            // Si no está logueado, redirigir a la página de inicio de sesión
-            return redirect()->to(base_url('login'));
-        }
+class Redireccion extends BaseController {
+    public function tienda($url) {
+        $this->redirigirSiNoLogueado('tienda');
     }
 
-    
-
-    public function profesores($url)
-    {
-        $session = session();
-        $usuario = $session->get('usuario');
-
-        if ($usuario) {
-            // Si el usuario está logueado, redirigir a la vista de profesores
-            return redirect()->to(base_url('profesores'));
-        } else {
-            // Si no está logueado, redirigir a la página de inicio de sesión
-            return redirect()->to(base_url('login'));
-        }
+    public function carrito($url) {
+        $this->redirigirSiNoLogueado('carrito');
     }
-    
-    public function panel($url)
-    {
+
+    public function rutinas($url) {
+        $this->redirigirSiNoLogueado('rutinas');
+    }
+
+    public function profesores($url) {
+        $this->redirigirSiNoLogueado('profesores');
+    }
+
+    public function panel($url) {
         $session = session();
         $usuario = $session->get('usuario');
         $tipo = $session->get('tipo');
-        $id = $session->get('id');
 
-        
         if ($usuario) {
-            // Si el usuario está logueado
             echo $tipo;
-            
-            if($tipo == 1) {
+
+            if ($tipo == 1) {
                 // Es un administrador
                 return redirect()->to(base_url('panel_admin'));
-
-            } 
+            }
             return redirect()->to(base_url('panel_cliente'));
-
         } else {
-            // Si no está logueado, redirigir a la página de inicio de sesión
-            return redirect()->to(base_url('login'));
+            $this->redirigirALogin();
         }
-        
-        
+    }
+
+    private function redirigirSiNoLogueado($destino) {
+        $session = session();
+        $usuario = $session->get('usuario');
+
+        if ($usuario) {
+            return redirect()->to(base_url($destino));
+        } else {
+            $this->redirigirALogin();
+        }
+    }
+
+    private function redirigirALogin() {
+        return redirect()->to(base_url('login'));
     }
 }
