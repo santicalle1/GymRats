105a3c555df1b0a7ffb0cf38684df13756040204
santicalle1 <santiagoferreyra@alumnos.itr3.edu.ar>
2023-12-02 16:07:15 -0300

    2/12/2023/3

--- app/Views/panel_admin.php
+++ app/Views/panel_admin.php
@@ -6,9 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel Administrador</title>
     <link rel="stylesheet" type="text/css" href="<?= base_url('css/panel_admin.css'); ?>">
-    <link rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script>
+        var inactivityTimeout; // Variable para almacenar el temporizador de inactividad
+
+        // Función para reiniciar el temporizador de inactividad
+        function resetInactivityTimeout() {
+            clearTimeout(inactivityTimeout); // Limpiamos el temporizador anterior
+            inactivityTimeout = setTimeout(logout, 180000); // 60000 ms = 1 minuto
+        }
+
+        // Función para redirigir a la página de cierre de sesión
+        function logout() {
+            window.location.href = '<?= base_url("inicio/logout"); ?>';
+        }
+
+        // Inicializa el temporizador de inactividad
+        resetInactivityTimeout();
+
+        // Agrega eventos de detección de actividad del usuario
+        document.addEventListener('mousemove', resetInactivityTimeout);
+        document.addEventListener('keydown', resetInactivityTimeout);
+    </script>
 </head>
+
+<body>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="dashboard.css">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <title>Pagina de panel de Admistrador</title>
+    </head>
+
     <body>
 
 
@@ -71,6 +106,18 @@
                         </a>
                     </li>
                     <li>
+                        <a href="<?= base_url('agregar_rutinas') ?>">
+                            <i class="fa fa-cog"></i>
+                            <span>Agregar Rutinas</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url('modificar_rutinas') ?>">
+                            <i class="fa fa-cog"></i>
+                            <span>Modificar Rutinas</span>
+                        </a>
+                    </li>
+                    <li>
                         <a href="<?= base_url('mensajes') ?>">
                             <i class="fa fa-cog"></i>
                             <span>Mensajes</span>
@@ -111,4 +158,10 @@
         </div>
 
     </body>
+
+    </html>
+
+
+</body>
+
 </html>
